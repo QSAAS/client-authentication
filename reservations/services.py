4573e84a097196c5523a4590657563f3f66f49be
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-07-24 05:25:08 +0200

    Add rabbit mq publishing of events

--- reservations/services.py
+++ reservations/services.py
@@ -1,2 +1,18 @@
+import json
+
+import environ
+import pika
+
+QUEUE_NAME = 'EventBus'
+
+env = environ.Env()
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(env.str("RABBIT_MQ_URL")))
+channel = connection.channel()
+
+channel.queue_declare(queue=QUEUE_NAME)
+
+
 def publish_event(obj):
-    pass
+    payload = json.dumps(obj)
+    channel.basic_publish(exchange='', routing_key='', body=bytes(payload))
