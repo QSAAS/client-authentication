cee03884129f9a633e373a556342d9c1d6be1e3a
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-07-24 05:27:53 +0200

    Add TODO

--- reservations/services.py
+++ reservations/services.py
@@ -14,5 +14,6 @@ channel.queue_declare(queue=QUEUE_NAME)
 
 
 def publish_event(obj):
+    # TODO: IMPORTANT: Add a key "eventName" to the event (obj) dictionary (from caller)
     payload = json.dumps(obj)
     channel.basic_publish(exchange='', routing_key='', body=bytes(payload))
