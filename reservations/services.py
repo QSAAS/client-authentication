355ae6a2708dd01bad7414567059ee1a223e76f0
Ahmed Hanafy <ahmed.hanafy@bld.ai>
2021-07-26 03:56:32 +0200

    Debug

--- reservations/services.py
+++ reservations/services.py
@@ -8,20 +8,23 @@ EXCHANGE_NAME = 'EventBus'
 env = environ.Env()
 
 connection = None
-channel = None
+channel2 = None
 
 
 def connect():
-    global connection, channel
+    global connection, channel2
     if connection is None or connection.is_closed:
         connection = pika.BlockingConnection(pika.URLParameters(env.str("RABBIT_MQ_URL")))
-        channel = connection.channel()
-        channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type='fanout')
-    return channel
+        channel2 = connection.channel()
+        channel2.exchange_declare(exchange=EXCHANGE_NAME, exchange_type='fanout')
+        print("connected xDD")
+    print("connected xD")
+    return channel2
 
 
 def publish_event(obj):
     channel = connect()
+    print(channel)
     # TODO: IMPORTANT: Add a key "eventName" to the event (obj) dictionary (from caller)
     obj['eventName'] = 'ReservationCreated'
     payload = json.dumps(obj, sort_keys=True, indent=1, cls=DjangoJSONEncoder)
